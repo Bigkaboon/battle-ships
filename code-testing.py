8feca4c76492e70c498f7a5df9226cea18e68397
Bigkaboon <>
2023-10-29 12:22:58 +0000

    added code to place boats randomly SOME fixes left...

--- code-testing.py
+++ code-testing.py
@@ -1,8 +1,16 @@
 import random
 
+
 def random_coordinates():
     """ Generates random coordinates for the computer ships start and end coordinates. """
-    direction_num = random.randint(1, 2)
+    computer_grid = []
+
+    for x in "ABCDEFG":
+        row = [x] + [" "] * 7
+        computer_grid.append(row)
+    
+    # direction_num = random.randint(1, 2)
+    direction_num = 2
 
     random_start_coordinates = ""
     random_end_coordinates = ""
@@ -16,6 +24,8 @@ def random_coordinates():
             random_end_coordinates = x_coordniate + str(random.randint(y_coordniate, 7))
             if random_start_coordinates != random_end_coordinates:
                 print(random_start_coordinates + " " + random_end_coordinates)
+
+                
     elif direction_num == 2:
         # vertical
         while random_start_coordinates == random_end_coordinates:
@@ -23,9 +33,23 @@ def random_coordinates():
             y_coordniate = random.randint(1, 7)
             random_start_coordinates = x_coordniate + str(y_coordniate)
             random_end_coordinates = random.choice(["A", "B", "C", "D", "E", "F", "G"]) + str(y_coordniate)
+            
+            
             if random_start_coordinates != random_end_coordinates:
                 print(random_start_coordinates + " " + random_end_coordinates)
-            
+                
+                start_coordinates = random_end_coordinates
+                end_coordinates = random_start_coordinates
+
+                start_x, start_y = ord(end_coordinates[0]) - ord("A"), int(end_coordinates[1]) - 1
+                end_x, end_y = ord(start_coordinates[0]) - ord("A"), int(start_coordinates[1]) - 1
+
+                for x in range(start_x, end_x + 1):
+                    for y in range(start_y, end_y + 1):
+                        computer_grid[x][y] = "X"
+                    
+                for row in computer_grid:
+                    print(row)
 
 
 random_coordinates()
