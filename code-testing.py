8d91c56b1eec7aded83eb8b9cefae978a4bd1734
Bigkaboon <>
2023-11-01 11:43:12 +0000

    removed the option for choosing direction and the length of the boats is max 1 coordinate wide and long

--- code-testing.py
+++ code-testing.py
@@ -1,16 +1,24 @@
 import random
 
+def counting_boats(computer_grid):
+    """ Counts the X's placed """
+    total_boat_parts = sum(row.count("X") for row in computer_grid)
+    print(f"The number of boat parts placed is {total_boat_parts}")
+    
 
 def random_coordinates():
     """ Generates random coordinates for the computer ships start and end coordinates. """
+    
+    total_boat_parts = 0
     computer_grid = []
 
     for x in "ABCDEFG":
-        row = [x] + [" "] * 7
+        row = [x] + [" "] * 8
         computer_grid.append(row)
     
-    # direction_num = random.randint(1, 2)
-    direction_num = 2
+        
+    direction_num = random.randint(1, 2)
+    
 
     random_start_coordinates = ""
     random_end_coordinates = ""
@@ -18,26 +26,51 @@ def random_coordinates():
         # horizontal
         while random_start_coordinates == random_end_coordinates:
             x_coordniate = random.choice(["A", "B", "C", "D", "E", "F", "G"])
-            y_coordniate = random.randint(1, 7)
+            y_coordniate = random.randint(2, 8)
             random_start_coordinates = x_coordniate + str(y_coordniate)
-            
+                
             random_end_coordinates = x_coordniate + str(random.randint(y_coordniate, 7))
             if random_start_coordinates != random_end_coordinates:
                 print(random_start_coordinates + " " + random_end_coordinates)
+                    
+                start_coordinates = random_end_coordinates
+                end_coordinates = random_start_coordinates
 
+                start_x, start_y = ord(end_coordinates[0]) - ord("A"), int(end_coordinates[1]) - 1
+                end_x, end_y = ord(start_coordinates[0]) - ord("A"), int(start_coordinates[1]) - 1
                 
+                for y in range(start_y, end_y + 1):
+                    for x in range(start_x, end_x + 1):
+                        computer_grid[x][y] = "X"
+                for row in computer_grid:
+                    print(row) 
+                    
+
+                    
     elif direction_num == 2:
         # vertical
+        my_list = ["A", "B", "C", "D", "E", "F", "G"]
+
         while random_start_coordinates == random_end_coordinates:
-            x_coordniate = random.choice(["A", "B", "C", "D", "E", "F", "G"])
-            y_coordniate = random.randint(1, 7)
+            x_coordniate = random.choice(my_list)
+            y_coordniate = random.randint(2, 8)
             random_start_coordinates = x_coordniate + str(y_coordniate)
-            random_end_coordinates = random.choice(["A", "B", "C", "D", "E", "F", "G"]) + str(y_coordniate)
-            
-            
+            end_x_coordinate = random.choice(my_list)
+
+            index_of_x = my_list.index(x_coordniate)
+            index_of_end_x = my_list.index(end_x_coordinate)
+
+            while index_of_end_x < index_of_x:
+                end_x_coordinate = random.choice(my_list)
+                if index_of_end_x > index_of_x:
+                    break
+
+            random_end_coordinates = end_x_coordinate + str(y_coordniate)
+                
+                
             if random_start_coordinates != random_end_coordinates:
                 print(random_start_coordinates + " " + random_end_coordinates)
-                
+                    
                 start_coordinates = random_end_coordinates
                 end_coordinates = random_start_coordinates
 
@@ -47,9 +80,14 @@ def random_coordinates():
                 for x in range(start_x, end_x + 1):
                     for y in range(start_y, end_y + 1):
                         computer_grid[x][y] = "X"
-                    
+                        
                 for row in computer_grid:
                     print(row)
+
+                    ### X's wont be shown if start x(G) > end x (A) = no X
+    counting_boats(computer_grid)
+        
+                
 
 
 random_coordinates()
--- run.py
+++ run.py
@@ -9,12 +9,15 @@ def start_game():
     """ Function for setting up the game """
     user_name = input("Select a username! \n")
     print(f"Hello {user_name}\n")
-    grid_use = grids()
-    player_grid = grid_use
-    computer_grid = grid_use
+
+    
+    player_grid = grids()
+    computer_grid = grids()
+    
     clear_console()
-    place_comp_ships(computer_grid)
+    random_boats(computer_grid)
     place_ships(player_grid)
+    
     
     
     
@@ -53,75 +56,52 @@ def grids():
 
 def place_ships(player_grid):
     """ Lets the user place X's representing the ships """
-    for i in range(3):
+
+    for i in range(10):
 
         for row in player_grid:
             print(row)
 
-        boat_direction = input("What direction do you want the boat to go 'vertical' or 'horizontal'?")
-        
-        if boat_direction == "vertical":
-            print("Place the boat vertically!")
-            start_coordinates = input("Write coordinates for where you want the boat to start")
-            end_coordinates = input("Write coordinates for where you want the boat to end")
+        x_coordinates = input("Write coordinates for where you want to place an 'X'")
 
-            start_x, start_y = ord(end_coordinates[0]) - ord("A"), int(end_coordinates[1]) - 1
-            end_x, end_y = ord(start_coordinates[0]) - ord("A"), int(start_coordinates[1]) - 1
+        x_row, x_col = ord(x_coordinates[0]) - ord("A"), int(x_coordinates[1]) - 1
 
-            for x in range(start_x, end_x + 1):
-                for y in range(start_y, end_y + 1):
-                    player_grid[x][y] = "X"
-                
-            for row in player_grid:
-                print(row)
-            
-            counting_boats(player_grid)
-        elif boat_direction == "horizontal":
-            print("Place the boat Horizontally!")
-            start_coordinates = input("Write coordinates for where you want the boat to start")
-            end_coordinates = input("Write coordinates for where you want the boat to end")
+        if 0 <= x_row < len(player_grid) and 0 <= x_col < len(player_grid[0]):
+            player_grid[x_row][x_col] = "X"
+        else:
+            print("Invalid coordinates for placing 'X'")
 
-            start_x, start_y = ord(end_coordinates[0]) - ord("A"), int(end_coordinates[1]) - 1
-            end_x, end_y = ord(start_coordinates[0]) - ord("A"), int(start_coordinates[1]) - 1
-        
-            for y in range(start_y, end_y + 1):
-                for x in range(start_x, end_x + 1):
-                    player_grid[x][y] = "X"
-            for row in player_grid:
-                print(row) 
+        for row in player_grid:
+            print(row)
     
-            counting_boats(player_grid)
-              
-                
-def counting_boats(player_grid):
+
+def counting_boats(player_grid, computer_grid):
     """ Counts the X's placed """
     total_boat_parts = sum(row.count("X") for row in player_grid)
     print(f"The number of boat parts placed is {total_boat_parts}")
 
 
-def random_coordinates():
+def random_boats(computer_grid):
     """ Generates random coordinates for the computer ships start and end coordinates. """
-    direction_num = random.randint(1, 2)
+    for i in range(10):
 
-    if direction_num == 1:
-        # horizontal
+        coordinate_list = []
+        
         x_coordniate = random.choice(["A", "B", "C", "D", "E", "F", "G"])
-        y_coordniate = random.randint(1, 7)
-        random_start_coordinates = x_coordniate + str(y_coordniate)
-        print(random_start_coordinates)
-        random_end_coordinates = x_coordniate + random.randint(y_coordniate, 7)
-    elif direction_num == 2:
-        # vertical
-        x_coordniate = random.choice(["A", "B", "C", "D", "E", "F", "G"])
-        y_coordniate = random.randint(1, 7)
-        random_start_coordinates = x_coordniate + str(y_coordniate)
-        random_end_coordinates = str(y_coordniate) + random.choice(["A", "B", "C", "D", "E", "F", "G"])
-        print(random_start_coordinates)
-        print(random_end_coordinates)
-def place_comp_ships(computer_grid,):
-    random_coordinates()
-    for row in computer_grid:
-        print(row)
+        y_coordniate = random.randint(2, 8)
+        random_boat_coordinates = x_coordniate + str(y_coordniate)
+        coordinate_list.append(random_boat_coordinates)
+
+        x_row, x_col = ord(random_boat_coordinates[0]) - ord("A"), int(random_boat_coordinates[1]) - 1
+
+        if 0 <= x_row < len(computer_grid) and 0 <= x_col < len(computer_grid[0]):
+            computer_grid[x_row][x_col] = "X"
+        else:
+            print("Invalid coordinates for placing 'X'")
+
+        for row in computer_grid:
+            print(row)
+    
 
 
 def main():
