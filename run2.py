d75d95043e2ff840360f3567cbbcbd3440a588ab
Bigkaboon <>
2023-11-04 20:22:36 +0000

    created get grid size function

--- run2.py
+++ run2.py
@@ -1,6 +1,9 @@
 import random
 
 user_name = ""
+grid_size = 7
+user_grid = []
+pc_grid = []
 
 
 def clear_console():
@@ -11,7 +14,26 @@ def clear_console():
 def validate_input(current_input, valid_options):
     return current_input in valid_options
 
+
 def get_user_name():
     user_name = input("Enter a username \n")
     while len(user_name) < 3:
         return user_name
+
+
+def get_grid_size():
+    grid_size = input("What grid size would you like to play on \n1. EASY : 7x7 \n2. MEDIUM : 9x9 \n3. HARD : 12x12 \nAnswer with a number 1-3 \n")
+    while validate_input(grid_size, ["1", "2", "3"]) is False:
+        grid_size = input("Invalid input, What grid size would you like to play on \n1. EASY : 7x7 \n2. MEDIUM : 9x9 \n3. HARD : 12x12 \nAnswer with a number 1-3 \n")  
+
+    if grid_size == "1":
+        grid_size = 7
+    if grid_size == "2":
+        grid_size = 9
+    if grid_size == "3":
+        grid_size == 12
+    return grid_size
+
+
+
+
