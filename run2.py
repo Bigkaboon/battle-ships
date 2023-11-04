bb3f1d2ad7054a440686ffcb65d5cf25fceecf75
Bigkaboon <>
2023-11-04 20:18:20 +0000

    added get username function, looking for a username < 3 charracters

--- run2.py
+++ run2.py
@@ -1,5 +1,17 @@
 import random
 
+user_name = ""
+
+
 def clear_console():
     """ clearing console once needed """
     print("\n" * 50)
+
+
+def validate_input(current_input, valid_options):
+    return current_input in valid_options
+
+def get_user_name():
+    user_name = input("Enter a username \n")
+    while len(user_name) < 3:
+        return user_name
