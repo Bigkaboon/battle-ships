92efa53954c826ee2406bcc321c39ed2f283556f
Bigkaboon <>
2023-11-04 21:22:22 +0000

    added code for placing boats

--- run2.py
+++ run2.py
@@ -4,6 +4,7 @@ user_name = ""
 grid_size = 7
 user_grid = []
 pc_grid = []
+random_coordinates = ""
 
 
 def clear_console():
@@ -49,7 +50,7 @@ def print_grid(grid, grid_size):
     number_list = "   "
     border = "***"
     while index <= grid_size:
-        number_list += f"{index}"
+        number_list += f"{index}  "
         index = index + 1
         border += "***"
     letter_list = "ABCDEFGHIJKL"
@@ -65,6 +66,78 @@ def print_grid(grid, grid_size):
     print(border)
 
 
+""" def generate_coordinates():
+    x_coordniate = random.choice(["A", "B", "C", "D", "E", "F", "G"])
+    y_coordniate = random.randint(2, 8)
+    random_coordinates = x_coordniate + str(y_coordniate)
+    return random_coordinates """
+    
+def place_ships_random(pc_grid):
+    for i in range(10):
+
+        x_coordniate = random.choice(["A", "B", "C", "D", "E", "F", "G"])
+        y_coordniate = random.randint(1, 7)
+        random_coordinates = x_coordniate + str(y_coordniate)
+        x_row, x_col = ord(random_coordinates[0]) - ord("A"), int(random_coordinates[1]) - 1
+
+        if 0 <= x_row < len(pc_grid) and 0 <= x_col < len(pc_grid[0]):
+            pc_grid[x_row][x_col] = "X"
+        else:
+            print("Invalid coordinates for placing 'X'")
+
+    print_grid(pc_grid, grid_size)
+
+
+def place_user_ships(user_grid):
+    """ Lets the user place X's representing the ships """
+    x = True
+    while x is True:
+        try:
+            want_random = int(input("How do you want you ships to be placed \n1. Randomly \n2. Manually\nAnswe 1 or 2."))
+            if want_random not in [1, 2]:
+                raise ValueError("Must enter either 1 or 2, please try again.")
+            
+        except ValueError as ve:
+            print(ve)
+        
+        if want_random == 2:
+            for i in range(10):
+
+    
+                boat_coordinates = input("Write coordinates for where you want to place an 'X'")
+                
+
+                x_row, x_col = ord(boat_coordinates[0]) - ord("A"), int(boat_coordinates[1]) - 1
+
+                if 0 <= x_row < len(user_grid) and 0 <= x_col < len(user_grid[0]):
+                    user_grid[x_row][x_col] = "X"
+                else:
+                    print("Invalid coordinates for placing 'X'")
+                print_grid(user_grid, grid_size)
+
+                
+            x = False
+        elif want_random == 1:
+            random_player_ships(user_grid, grid_size)
+            x = False
+
+
+def random_player_ships(user_grid, grid_size):
+    for i in range(10):
+        x_coordniate = random.choice(["A", "B", "C", "D", "E", "F", "G"])
+        y_coordniate = random.randint(1, 7)
+        random_boat_coordinates = x_coordniate + str(y_coordniate)
+            
+            
+        x_row, x_col = ord(random_boat_coordinates[0]) - ord("A"), int(random_boat_coordinates[1]) - 1
+
+        if 0 <= x_row < len(user_grid) and 0 <= x_col < len(user_grid[0]):
+            user_grid[x_row][x_col] = "X"
+        else:
+            print("Invalid coordinates for placing 'X'")
+
+    print_grid(user_grid, grid_size)
+
 def start_game():
     user_name = get_user_name()
     print(f"Hello {user_name}\n")
@@ -72,5 +145,9 @@ def start_game():
     user_grid = create_empty_grid(grid_size)
     pc_grid = create_empty_grid(grid_size)
     print_grid(user_grid, grid_size)
+    place_ships_random(pc_grid)
+    place_user_ships(user_grid)
 
+    
 
+start_game()
