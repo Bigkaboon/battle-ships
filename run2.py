d52e65ec362f9e020661ad586cd6e0514398e4f1
Bigkaboon <>
2023-11-05 14:54:28 +0000

    added functions generating guesses and for the user to guess

--- run2.py
+++ run2.py
@@ -6,6 +6,9 @@ user_grid = []
 pc_grid = []
 random_coordinates = ""
 letter_list = "ABCDEFGHIJKL"
+p_guess = ""
+pc_guess = ""
+
 
 
 def clear_console():
@@ -40,7 +43,7 @@ def get_grid_size():
 def get_user_ships_mode():
     want_random = 0
     while want_random  not in ["1", "2"]:
-        want_random = inmput("How do you want you ships to be placed \n1. Randomly \n2. Manually\nAnswer 1 or 2.")
+        want_random = input("How do you want you ships to be placed \n1. Randomly \n2. Manually\nAnswer 1 or 2.")
         if validate_input(want_random, ["1", "2"]) is False:
             print("Must enter either 1 or 2, please try again.")
     return want_random
@@ -78,11 +81,10 @@ def print_grid(grid, grid_size):
 def place_random_ships(grid, grid_size):
     ships_placed = 0
     while ships_placed < grid_size:
-
-        x_coordniate = random.randint(0, grid_size - 1)
-        y_coordniate = random.randint(0, grid_size - 1)
-        if (grid[x_coordniate][y_coordniate] == 0):
-            grid[x_coordniate][y_coordniate] == "X"
+        x_coordinate = random.randint(0, grid_size - 1)
+        y_coordinate = random.randint(0, grid_size - 1)
+        if (grid[x_coordinate][y_coordinate] == 0):
+            grid[x_coordinate][y_coordinate] = "X"
             ships_placed += 1
     return grid
 
@@ -99,6 +101,9 @@ def translate_coordinates(coordinate, grid_size):
             int_input_number = int(input_number)
             if int_input_number <= grid_size and int_input_number >= 1:
                 return (valid_letters.index(letter.upper()), int_input_number - 1)
+            else:
+                print(f"Please enter a number >=1 and <={grid_size}")
+                return False
         except ValueError:
             print(f"Please enter a number for the second coordinate")
             False
@@ -112,7 +117,7 @@ def place_ships_manually(grid, grid_size):
             coordinate = input("Enter a coordinate (A6):")
             translated_coordinate = translate_coordinates(coordinate, grid_size)
             if translated_coordinate is not False:
-                if (grid[translated_coordinate[0]][translated_coordinate[1]] = 0):
+                if (grid[translated_coordinate[0]][translated_coordinate[1]] == 0):
                     grid[translated_coordinate[0]][translated_coordinate[1]] = "X"
                     ships_placed += 1
                     valid_coordinate = True
@@ -121,38 +126,14 @@ def place_ships_manually(grid, grid_size):
     return grid
 
 
-
 def place_user_ships(user_grid, grid_size):
     """ Lets the user place X's representing the ships """
-    x = True
-    while x is True:
-        try:
-            want_random = int(input("How do you want you ships to be placed \n1. Randomly \n2. Manually\nAnswe 1 or 2."))
-            if want_random not in [1, 2]:
-                raise ValueError("Must enter either 1 or 2, please try again.")
-            
-        except ValueError as ve:
-            print(ve)
-        
-        if want_random == 2:
-            for i in range(10):
-                
-    
-                boat_coordinates = input("Write coordinates for where you want to place an 'X'")
-                
-
-                x_row, x_col = ord(boat_coordinates[0]) - ord("A"), int(boat_coordinates[1]) - 1
-                
-
-                if 0 <= x_row < len(user_grid) and 0 <= x_col < len(user_grid[0]):
-                    user_grid[x_row][x_col] = "X"
-                else:
-                    print("Invalid coordinates for placing 'X'")
-                print_grid(user_grid, grid_size)
-            x = False
-        elif want_random == 1:
-            random_player_ships(user_grid, grid_size)
-            x = False
+    ship_mode = get_user_ships_mode()
+    if ship_mode == "1":
+        user_grid = place_random_ships(user_grid, grid_size)
+    if ship_mode == "2":
+        place_ships_manually(user_grid, grid_size)
+    return user_grid
 
 
 def random_player_ships(user_grid, grid_size):
@@ -180,18 +161,78 @@ def random_player_ships(user_grid, grid_size):
 
     print_grid(user_grid, grid_size)
 
+
+def player_guess():
+    translated_p_guess = False
+    while translated_p_guess is False:
+        p_guess = input("Guess a coordinate (A6): ")
+        translated_p_guess = translate_coordinates(p_guess, grid_size)
+        if translated_p_guess is not False:
+            print("Your guess is valid")
+            print(f"Your guess is {p_guess}")
+            translated_p_guess = True
+        else:
+            print("Your guess is not valid")
+    return p_guess
+    
+
+
+def computer_guess():
+    x_coordinate = random.randint(0, grid_size - 1)
+    y_coordinate = random.randint(0, grid_size - 1)
+    x_letter_coordinate = letter_list[x_coordinate]
+
+    pc_guess = x_letter_coordinate + str(y_coordinate)
+    print(pc_guess)
+    return pc_guess
+    
+
+def handle_computer_guess(grid):
+    guess = computer_guess()
+    translated_guess = translate_coordinates(guess, grid_size)
+    if translated_guess is not False:
+        if (grid[translated_guess[0]][translated_guess[1]] == "X"):
+            grid[translated_guess[0]][translated_guess[1]] = 1
+            print("Computer guessed right")
+        else:
+            grid[translated_guess[0]][translated_guess[1]] = 2
+            print("Comupter guessed wrong")
+
+
+def handle_player_guess(grid):
+    guess = player_guess()
+    translated_guess = translate_coordinates(guess, grid_size)
+    if translated_guess is not False:
+        if (grid[translated_guess[0]][translated_guess[1]] == "X"):
+            grid[translated_guess[0]][translated_guess[1]] = 1
+            print("You guessed right")
+        else:
+            grid[translated_guess[0]][translated_guess[1]] = 2
+            print("Wrong guess")
+
+            
+def play_game():
+    handle_player_guess(pc_grid)
+    print_grid(pc_grid, grid_size)
+    
+
+    
+
+
 def start_game():
     user_name = get_user_name()
     print(f"Hello {user_name}\n")
     grid_size = get_grid_size()
     user_grid = create_empty_grid(grid_size)
     pc_grid = create_empty_grid(grid_size)
+    pc_grid = place_random_ships(pc_grid, grid_size)
+    print_grid(pc_grid, grid_size)
+    user_grid = place_user_ships(user_grid, grid_size)
+    print_grid(user_grid, grid_size)
+    handle_computer_guess(user_grid)
     print_grid(user_grid, grid_size)
     
-    place_computer_ships(pc_grid, grid_size)
-    place_user_ships(user_grid,grid_size)
     
-
     
 if __name__ == "__main__":
     start_game()
