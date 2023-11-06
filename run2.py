0b2311edda36ada44314121b4f9e4255972d034c
Bigkaboon <>
2023-11-06 09:14:21 +0000

    Fixed all the errors given by pep8

--- run2.py
+++ run2.py
@@ -27,14 +27,22 @@ def validate_input(current_input, valid_options):
 def get_user_name():
     user_name = input("Enter a username \n")
     while len(user_name) < 3:
-        user_name = input("Usernames should be at least 3 chars long, please enter a username! \n")
+        user_name = input("""Usernames should be at least 3 chars long,
+        please enter a username! \n""")
     return user_name
 
 
 def get_grid_size():
-    grid_size = input("What grid size would you like to play on \n1. EASY : 7x7 \n2. MEDIUM : 9x9 \n3. HARD : 12x12 \nAnswer with a number 1-3 \n")
+    grid_size = input("""What grid size would you like to play on \n
+    1. EASY : 7x7 \n
+    2. MEDIUM : 9x9 \n
+    3. HARD : 12x12 \nAnswer with a number 1-3 \n""")
     while validate_input(grid_size, ["1", "2", "3"]) is False:
-        grid_size = input("Invalid input, What grid size would you like to play on \n1. EASY : 7x7 \n2. MEDIUM : 9x9 \n3. HARD : 12x12 \nAnswer with a number 1-3 \n")
+        grid_size = input("""Invalid input, What grid size would
+        you like to play on \n
+        1. EASY : 7x7 \n
+        2. MEDIUM : 9x9 \n
+        3. HARD : 12x12 \nAnswer with a number 1-3 \n""")
     if grid_size == "1":
         grid_size = 7
     if grid_size == "2":
@@ -46,8 +54,10 @@ def get_grid_size():
 
 def get_user_ships_mode():
     want_random = 0
-    while want_random  not in ["1", "2"]:
-        want_random = input("How do you want you ships to be placed \n1. Randomly \n2. Manually\nAnswer 1 or 2.")
+    while want_random not in ["1", "2"]:
+        want_random = input("""How do you want you ships to be placed \n
+        1. Randomly \n
+        2. Manually\nAnswer 1 or 2.""")
         if validate_input(want_random, ["1", "2"]) is False:
             print("Must enter either 1 or 2, please try again.")
     return want_random
@@ -76,12 +86,12 @@ def print_grid(grid, grid_size):
     for row in grid:
         row_list = f"{letter_list[index]}  "
         for item in row:
-           row_list += f"{item}  "
+            row_list += f"{item}  "
         print(f"{row_list}\n")
         index = index + 1
     print(border)
 
-    
+
 def place_random_ships(grid, grid_size):
     ships_placed = 0
     while ships_placed < grid_size:
@@ -104,13 +114,16 @@ def translate_coordinates(coordinate, grid_size):
         try:
             int_input_number = int(input_number)
             if int_input_number <= grid_size and int_input_number >= 1:
-                return (valid_letters.index(letter.upper()), int_input_number - 1)
+                return (valid_letters.index(letter.upper()),
+                        int_input_number - 1)
             else:
                 print(f"Please enter a number >=1 and <={grid_size}")
                 return False
         except ValueError:
             print(f"Please enter a number for the second coordinate")
             False
+    if coordinate[1:] is not int:
+        return False
 
 
 def place_ships_manually(grid, grid_size):
@@ -120,15 +133,21 @@ def place_ships_manually(grid, grid_size):
         while valid_coordinate is False:
             coordinate = input("Enter a coordinate (A6):")
             if len(coordinate) <= 1:
-                coordinate = input("Input can't be empty! Please enter a coordinate (A6):")
-            translated_coordinate = translate_coordinates(coordinate, grid_size)
+                coordinate = input("""Input can't be empty!
+                Please enter a coordinate (A6):""")
+            translated_coordinate = translate_coordinates(
+                                        coordinate, grid_size)
+            tr_coord = translated_coordinate
             if translated_coordinate is not False:
-                if (grid[translated_coordinate[0]][translated_coordinate[1]] == "_"):
-                    grid[translated_coordinate[0]][translated_coordinate[1]] = "X"
+                if (grid[translated_coordinate[0]]
+                        [translated_coordinate[1]]
+                        == "_"):
+                    grid[tr_coord[0]][tr_coord[1]] = "X"
                     ships_placed += 1
                     valid_coordinate = True
                 else:
-                    print(f"The current coordinate already has a ship, please enter a different one")
+                    print(f"""The current coordinate already has a ship,
+                    please enter a different one""")
     return grid
 
 
@@ -142,38 +161,13 @@ def place_user_ships(user_grid, grid_size):
     return user_grid
 
 
-def random_player_ships(user_grid, grid_size):
-    letters = ["A", "B", "C", "D", "E", "F", "G"]
-    x = 7
-    if grid_size == 9:
-        letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-        x = 9
-    elif grid_size == 12:
-        letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
-        x = 12
-
-    for i in range(10):
-        x_coordniate = random.choice(letters)
-        y_coordniate = random.randint(1, x)
-        random_boat_coordinates = x_coordniate + str(y_coordniate)
-            
-            
-        x_row, x_col = ord(random_boat_coordinates[0]) - ord("A"), int(random_boat_coordinates[1]) - 1
-
-        if 0 <= x_row < len(user_grid) and 0 <= x_col < len(user_grid[0]):
-            user_grid[x_row][x_col] = "X"
-        else:
-            print("Invalid coordinates for placing 'X'")
-
-    print_grid(user_grid, grid_size)
-
-
-def player_guess():
+def player_guess(grid_size):
     translated_p_guess = False
     while translated_p_guess is False:
         p_guess = input("Guess a coordinate (A6): ")
         while len(p_guess) <= 1 or "":
-            p_guess = input("Input can't be empty! Please guess a coordinate (A6): ")
+            p_guess = input("""Input can't be empty!
+                Please guess a coordinate (A6): """)
         translated_p_guess = translate_coordinates(p_guess, grid_size)
         if translated_p_guess is not False:
             print(f"Your guess is {p_guess}")
@@ -187,7 +181,7 @@ def print_line():
     print("-------------------------\n")
 
 
-def computer_guess():
+def computer_guess(grid_size):
     x_coordinate = random.randint(0, grid_size - 1)
     y_coordinate = random.randint(1, grid_size - 1)
     x_letter_coordinate = letter_list[x_coordinate]
@@ -195,12 +189,12 @@ def computer_guess():
     pc_guess = x_letter_coordinate + str(y_coordinate)
     print(f"Computer guessed {pc_guess}")
     return pc_guess
-    
 
-def handle_computer_guess(grid):
-    guess = computer_guess()
+
+def handle_computer_guess(grid, grid_size):
+    guess = computer_guess(grid_size)
     while guess in pc_wrong_guess:
-        guess = computer_guess()
+        guess = computer_guess(grid_size)
     translated_guess = translate_coordinates(guess, grid_size)
     if translated_guess is not False:
         if (grid[translated_guess[0]][translated_guess[1]] == "X"):
@@ -218,11 +212,11 @@ def show_guesses(wrong_guess, right_guess):
     print(f"This is the right guesses:\n{right_guess}\n")
 
 
-def handle_player_guess(grid):
-    guess = player_guess()
+def handle_player_guess(grid, grid_size):
+    guess = player_guess(grid_size)
     while guess in p_wrong_guess:
         print("You have already guessed that coordinate")
-        guess = player_guess()
+        guess = player_guess(grid_size)
     translated_guess = translate_coordinates(guess, grid_size)
     if translated_guess is not False:
         if (grid[translated_guess[0]][translated_guess[1]] == "X"):
@@ -244,16 +238,17 @@ def show_stats(user_name, grid_size):
     print(f"First to {grid_size}")
 
 
-def determine_winner(winner, p_right_guess, pc_right_guess, grid_size, user_name):
+def determine_winner(
+        winner, p_right_guess, pc_right_guess, grid_size, user_name):
     if len(p_right_guess) == grid_size:
         print(f"Congrats {user_name}! You Win!.")
         return True
     elif len(pc_right_guess) == grid_size:
         print("GAME OVER! The computer wins.")
-        return True   
-    else: 
+        return True
+    else:
         winner = False
-     
+
 
 def start_game():
     user_name = get_user_name()
@@ -278,22 +273,19 @@ def start_game():
         show_guesses(p_wrong_guess, p_right_guess)
         print_line()
         print_grid(user_grid, grid_size)
-        handle_player_guess(pc_grid)
-        determine_winner(winner, p_right_guess, pc_right_guess, grid_size, user_name)
-        handle_computer_guess(user_grid)
+        handle_player_guess(pc_grid, grid_size)
+        determine_winner(
+            winner, p_right_guess, pc_right_guess, grid_size, user_name)
+        handle_computer_guess(user_grid, grid_size)
         print_grid(user_grid, grid_size)
         show_stats(user_name, grid_size)
-        winner = determine_winner(winner, p_right_guess, pc_right_guess, grid_size, user_name)
-        
+        winner = determine_winner(
+            winner, p_right_guess, pc_right_guess, grid_size, user_name)
+
         if winner is True:
             print("Game has ended")
             break
-    
-        
-    
-    
-    
-    
+
+
 if __name__ == "__main__":
     start_game()
-    
